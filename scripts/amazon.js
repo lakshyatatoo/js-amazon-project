4e9d0ed94d96b3feee229be7fb8030169faf2b6f
lakshya_tatoo <lakshyatatoo31@gmai.com>
2025-08-03 10:10:46 +0530

    product stored in a file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,33 +1,4 @@
-const products = [
-  {
-    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-    name: " Black and Gray Athletic Cotton Socks - 6 Pairs",
-    rating: {
-      stars: 4.5,
-      reviews: 87,
-    },
-    priceCent: 1090,
-  },
-  {
-    image: "images/products/intermediate-composite-basketball.jpg",
-    name: "Intermediate Size Basketball",
-    rating: {
-      stars: 4,
-      reviews: 127,
-    },
-    priceCent: 2095,
-  },
-  {
-    image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-    name: "Adults Plain Cotton T-Shirt - 2 Pack",
-    rating: {
-      stars: 4.5,
-      reviews: 56,
-    },
-    priceCent: 799,
-  },
-];
-
+//
 //adding a string each time we loop throguh a array to create all product combined html string
 let productHtml = "";
 products.forEach((product) => {
@@ -49,11 +20,13 @@ products.forEach((product) => {
               src="images/ratings/rating-${product.rating.stars * 10}.png"
             />
             <div class="product-rating-count link-primary">${
-              product.rating.reviews
+              product.rating.count
             }</div>
           </div>
 
-          <div class="product-price">${product.priceCent / 100}</div>
+          <div class="product-price">${(product.priceCents / 100).toFixed(
+            2
+          )}</div>
 
           <div class="product-quantity-container">
             <select>
