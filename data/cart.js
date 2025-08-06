cf7f13c920614b4a4c7a594d8d93d4bfe1897407
lakshya_tatoo <lakshyatatoo31@gmai.com>
2025-08-07 00:37:10 +0530

    generating html for product in checkout page

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,14 @@
-export const cart = []; //help access varibales outside the file
+export const cart = [
+  {
+    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    quantity: 2,
+  },
+  {
+    //default values for basic
+    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    quantity: 2,
+  },
+]; //help access varibales outside the file
 
 export function addtoocart(productId) {
   const quantitydata = document.querySelector(
